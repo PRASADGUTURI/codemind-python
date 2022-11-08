d9354ab5f2ef20332bbfc20324a188d246c7caa8
PRASADGUTURI <102518969+PRASADGUTURI@users.noreply.github.com>
2022-11-08 20:03:31 +0530

    n=int(input()) l=list(map(int,input().split())) s=0 for i in range(len(l)):     if i%2!=0 and l[i]%2!=0:         s=s+1 v=[i for i in l if i%2!=0] if s==len(v):     print(True) else:     print(False)

---
+++ Strictly_ODD.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+s=0
+for i in range(len(l)):
+    if i%2!=0 and l[i]%2!=0:
+        s=s+1
+v=[i for i in l if i%2!=0]    
+if s==len(v):
+    print(True)
+else:
+    print(False)
